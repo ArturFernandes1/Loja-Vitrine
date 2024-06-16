b7c4c8a402d3bd514f331c240809228680f218cf
Artur Dantas <arturdantas121@gmail.com>
2024-06-16 19:02:48 -0300

    first commit

--- src/Components/NewsNavbar.js
+++ src/Components/NewsNavbar.js
@@ -1,6 +1,6 @@
-
 import React, { useState } from 'react';
-import './NewsNavbar.css'; // Assegure-se de ter este arquivo de estilos na mesma pasta ou ajuste o caminho conforme necessário
+import { Link } from 'react-router-dom'; // Importe o Link do React Router
+import './NewsNavbar.css';
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -13,18 +13,21 @@ const Navbar = () => {
         <header>
             <div className={`topnav${isOpen ? ' responsive' : ''}`} id="myTopnav">
                 <h1 className="titleh1"><samp id="spam1">PAI</samp> <samp id="spam2">&FILHO</samp></h1>
-                <a href="/">HOME</a>
-                <a href="/Empresa">EMPRESA</a>
-                <a href="/Contato">CONTATO</a>
-                <a href="/Portifolio">PORTIFÓLIO</a>
-                <a href="/Orcamento"  id='item-last'>ORÇAMENTO</a>
-                <a href="#" className="icon" onClick={toggleMenu}>
+                <Link to="/">HOME</Link>
+                <Link to="/Empresa">EMPRESA</Link>
+                <Link to="/Contato">CONTATO</Link>
+                <Link to="/Portifolio">PORTIFÓLIO</Link>
+                <Link to="/Orcamento" id='item-last'>ORÇAMENTO</Link>
+        {/*         <a href="#" className="icon" onClick={toggleMenu}>
                     <span>&#9776;</span>
-                </a>
+                </a> */}
+                <Link to="#" className="icon" onClick={toggleMenu}>
+                <span>&#9776;</span>
+                </Link>
                 
             </div>
         </header>
     );
 };
 
-export default Navbar; 
+export default Navbar;
